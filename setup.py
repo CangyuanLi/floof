52a78cc24f1a3056f3fd33ab4f90579e61179b44
CangyuanLi <everest229@gmail.com>
2023-02-14 18:06:25 -0500

    version 0.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="floof",
-    version="0.0.8",
+    version="0.0.9",
     author="Cangyuan Li",
     author_email="everest229@gmail.com",
     description="A library for fuzzymatching",
