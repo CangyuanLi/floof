0786665cfa1cf472c18edae6fcf11ac799b4b4a9
CangyuanLi <everest229@gmail.com>
2023-03-05 18:03:22 -0500

    check that single-threaded version works

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -9,10 +9,10 @@ DAT_PATH = Path(__file__).resolve().parents[0] / "data"
 
 def main():
     df = pd.read_csv(DAT_PATH / "test_data.csv")
-    df = df.sample(10_000)
+    df = df.sample(30_000)
 
     matcher = floof.Matcher(df["name1"], df["name2"])
-    cutils.time_func(matcher.hamming, warmups=1, iterations=10)
+    cutils.time_func(lambda: matcher.levenshtein(ncpus=1), warmups=1, iterations=10)
 
 if __name__ == "__main__":
     main()
