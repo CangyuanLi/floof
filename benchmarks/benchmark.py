04d626e04d2216c46f6ee7b590d9675e3f3b2b45
CangyuanLi <everest229@gmail.com>
2023-03-20 10:42:54 -0400

    benchmarks

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -9,12 +9,12 @@ DAT_PATH = Path(__file__).resolve().parents[0] / "data"
 
 def main():
     df = pd.read_csv(DAT_PATH / "test_data.csv")
-    df = df.sample(2_000)
+    df = df.sample(10_000)
 
     matcher = floof.Matcher(df["name1"], df["name2"])
 
-    cutils.time_func(lambda: matcher.hamming(ncpus=2), warmups=1, iterations=2)
-    cutils.time_func(lambda: matcher.hamming(ncpus=2, ascii_only=True), warmups=1, iterations=2)
+    cutils.time_func(lambda: matcher.hamming(ncpus=1), warmups=1, iterations=2)
+    cutils.time_func(lambda: matcher.hamming(ncpus=1, ascii_only=True), warmups=1, iterations=2)
 
 
 if __name__ == "__main__":
