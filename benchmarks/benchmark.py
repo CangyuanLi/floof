3fc6fc4e2fd0318efc795e9ebe4474752341fdf6
CangyuanLi <everest229@gmail.com>
2023-03-22 19:18:33 -0400

    benchmarks

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,20 +1,37 @@
 # Imports
 from pathlib import Path
 
-import cutils
 import floof
 import pandas as pd
+import perfplot
 
 DAT_PATH = Path(__file__).resolve().parents[0] / "data"
 
+def data_setup(df: pd.DataFrame, n: int) -> floof.Matcher:
+    df = df.sample(n)
+
+    return floof.Matcher(df["name1"], df["name2"])
+
 def main():
     df = pd.read_csv(DAT_PATH / "test_data.csv")
-    df = df.sample(10_000)
 
-    matcher = floof.Matcher(df["name1"], df["name2"])
-
-    cutils.time_func(lambda: matcher.hamming(ncpus=1), warmups=1, iterations=2)
-    cutils.time_func(lambda: matcher.hamming(ncpus=1, ascii_only=True), warmups=1, iterations=2)
+    perfplot.save(
+        filename=Path(__file__).resolve().parents[0] / "edit_distance_scorers.png",
+        setup=lambda n: data_setup(df, n),
+        n_range=[2**k for k in range(10)],
+        kernels=[
+            lambda m: m.hamming(),
+            lambda m: m.hamming(ascii_only=True),
+            lambda m: m.jaro(),
+            lambda m: m.jaro_winkler(),
+            lambda m: m.levenshtein(),
+            lambda m: m.damerau_levenshtein()
+        ],
+        time_unit="auto",
+        labels=["hamming", "hamming (ascii)", "jaro", "jaro_winkler", "levenshtein", "damerau_levenshtein"],
+        xlabel="Number of rows",
+        equality_check=None,
+    )
 
 
 if __name__ == "__main__":
