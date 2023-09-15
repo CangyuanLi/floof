92a2e07b5f774371a1ba4f2eb15de1cdee59997f
Cangyuan Li <everest229@gmail.com>
2023-09-15 13:37:33 -0400

    add function pointer type

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,26 @@
+use pyo3::exceptions::PyException;
 use smallvec::SmallVec;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum FloofError {
+    #[error(transparent)]
+    ThreadPoolError(#[from] rayon::ThreadPoolBuildError),
+}
+
+impl std::convert::From<FloofError> for pyo3::PyErr {
+    fn from(err: FloofError) -> pyo3::PyErr {
+        match err {
+            FloofError::ThreadPoolError(_) => PyException::new_err(err.to_string()),
+        }
+    }
+}
 
 const VEC_SIZE: usize = 32;
 
 pub type FastVec<T> = SmallVec<[T; VEC_SIZE]>;
+
+pub type SimilarityFunc = fn(&str, &str) -> f64;
 
 pub trait HasLength {
     fn len(&self) -> usize;
@@ -27,3 +45,15 @@ pub fn distance_to_similarity(distance: usize, len1: usize, len2: usize) -> f64 
 
     (max_len - dist) / max_len
 }
+
+pub fn create_rayon_pool(num_threads: usize) -> Result<rayon::ThreadPool, FloofError> {
+    match rayon::ThreadPoolBuilder::new()
+        .num_threads(num_threads)
+        .build()
+    {
+        Err(e) => Err(e.into()),
+        Ok(pool) => Ok(pool),
+    }
+}
+
+pub fn resolve_njobs(n_jobs: i32) {}
