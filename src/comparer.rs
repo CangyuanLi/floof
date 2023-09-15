a1f6c250de850866e6026de8ef2aca2e1cdaa08f
Cangyuan Li <everest229@gmail.com>
2023-09-15 13:44:03 -0400

    use par_iter instead of into_par_iter for consistency

--- src/comparer.rs
+++ src/comparer.rs
@@ -1,12 +1,13 @@
 use rayon::prelude::*;
 
 use crate::utils;
+use indicatif::ParallelProgressIterator;
 
 pub fn fuzzycompare(arr1: &[&str], arr2: &[&str], func: utils::SimilarityFunc) -> Vec<f64> {
     assert_eq!(arr1.len(), arr2.len());
 
     let res: Vec<f64> = arr1
-        .into_par_iter()
+        .par_iter()
         .zip(arr2)
         .map(|(s1, s2)| func(s1, s2))
         .collect();
