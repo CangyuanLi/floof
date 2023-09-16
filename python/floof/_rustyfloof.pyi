1ce3576b3f67b16c33b5ec8daed343617de099d9
Cangyuan Li <everest229@gmail.com>
2023-09-15 23:56:37 -0400

    docstrings

--- python/floof/_rustyfloof.pyi
+++ python/floof/_rustyfloof.pyi
@@ -2,8 +2,48 @@ from typing import Literal
 
 RustScorers = Literal["hamming", "hamming_ascii", "jaccard", "jaccard_ascii"]
 
-def hamming(s1: str, s2: str, ascii_only: bool = False) -> float: ...
-def jaccard(s1: str, s2: str, ascii_only: bool = False) -> float: ...
+def hamming(s1: str, s2: str, ascii_only: bool = False) -> float:
+    """Calculates the extend Hamming similarity between two strings. The Hamming
+    distance is defined as the total number of differences.
+
+    Parameters
+    ----------
+    s1 : str
+    s2 : str
+    ascii_only : bool, optional
+        If the string contains only ASCII characters, avoids the (expensive) operation
+        of creating Unicode graphemes, by default False
+
+    Returns
+    -------
+    float
+        The Hamming distance scaled by the lengths of the strings
+    """
+    ...
+
+def jaccard(s1: str, s2: str, ascii_only: bool = False) -> float:
+    r"""Calculates the Jaccard similarity between two strings. The Jaccard similarity
+    is a metric for the proximity between two sets, and is given by:
+
+    ..math::
+
+        J(A, B) = \frac{|A \cap B|}{|A \cup B|}
+
+    Parameters
+    ----------
+    s1 : str
+    s2 : str
+    ascii_only : bool, optional
+        If the string contains only ASCII characters, avoids the (expensive) operation
+        of creating Unicode graphemes, by default False
+
+    Returns
+    -------
+    float
+        The Jaccard similarity
+    """
+    ...
+
 def _compare(
     arr1: list[str], arr2: list[str], func_name: RustScorers, n_jobs: int = 0
 ) -> list[float]: ...
