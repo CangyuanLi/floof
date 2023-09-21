18dae7904139258f4a077c7606ab87b34c659c5d
Cangyuan Li <everest229@gmail.com>
2023-09-20 22:43:21 -0400

    benchmark

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -51,8 +51,8 @@ def create_unicode_db():
 
 
 def create_ascii_db():
-    col1 = [random_unicode_string(random.randint(0, 100)) for _ in range(1_000_000)]
-    col2 = [random_unicode_string(random.randint(0, 100)) for _ in range(1_000_000)]
+    col1 = [random_ascii_string(random.randint(0, 100)) for _ in range(1_000_000)]
+    col2 = [random_ascii_string(random.randint(0, 100)) for _ in range(1_000_000)]
 
     pl.DataFrame(data={"col1": col1, "col2": col2}).write_parquet(
         DAT_PATH / "ascii.parquet"
@@ -61,10 +61,15 @@ def create_ascii_db():
 
 def main():
     df = pl.scan_parquet(DAT_PATH / "ascii.parquet").fetch(100_000)
-    matcher = floof.Matcher(df.get_column("col1"), df.get_column("col2"))
+    matcher_unicode = floof.Matcher(df.get_column("col1"), df.get_column("col2"))
+    matcher_ascii = floof.Matcher(
+        df.get_column("col1"), df.get_column("col2"), ascii_only=True
+    )
 
-    # matcher._get_all_matches_rust("levenshtein_ascii", k_matches=5, threshold=0)
-    matcher.levenshtein()
+    # matcher_unicode._get_all_matches_rust("hamming_ascii", k_matches=5, threshold=0)
+    # matcher_unicode._get_all_matches_rust("jaro_ascii", k_matches=5, threshold=0)
+    # matcher_ascii.jaro()
+    matcher_unicode.jaro()
 
 
 if __name__ == "__main__":
